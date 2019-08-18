ef35c540518443364d288578ea2e129fab29e0a7
Taylor Hodge <j.taylor.hodge@gmail.com>
2019-08-18 13:57:35 -0400

    sicp ex1.3

--- u00/0-1/sicp1-1.rkt
+++ u00/0-1/sicp1-1.rkt
@@ -19,3 +19,12 @@ Empty return; Defines procedure of assigning value of `a + 1` to `b` (4)
 
 ;; Exercise 1.2
 (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7)))
+
+;; Exercise 1.3
+(define (square x) (* x x))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
+(define (largest-sum-of-squares x y z)
+  (sum-of-squares (max x y) (max (min x y) z)))
