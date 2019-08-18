d60e33d8c20a4ea739bde4e687e9f85c82761ef9
Taylor Hodge <j.taylor.hodge@gmail.com>
2019-08-18 12:54:18 -0400

    sicp ex1.1

---
+++ u00/0-1/sicp1-1.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+;; Exercise 1.1
+#|
+10
+12
+8
+3
+6
+Empty return; Defines procedure of assigning value `3` to `a`
+Empty return; Defines procedure of assigning value of `a + 1` to `b` (4)
+19
+#f
+4
+16
+6
+16
+|#
+
